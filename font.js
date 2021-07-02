b0f99bfb0b4c9a56362647fea8c20a005c566512
Giancarlo Antonucci <antonuccig@icloud.com>
2021-07-02 13:59:37 +0100

    Add `font.js`

---
+++ font.js
@@ -0,0 +1,33 @@
+function nativeSelector() {
+    var elements = document.querySelectorAll("can, can *");
+    var results = [];
+    var child;
+    for(var i = 0; i < elements.length; i++) {
+        child = elements[i].childNodes[0];
+        if(elements[i].hasChildNodes() && child.nodeType == 3) {
+            results.push(child);
+        }
+    }
+    return results;
+}
+
+var textnodes = nativeSelector(),
+    _nv;
+for (var i = 0, len = textnodes.length; i<len; i++){
+    _nv = textnodes[i].nodeValue;
+    textnodes[i].nodeValue = _nv.replace(/e</g,'\uE014');
+}
+
+var textnodes = nativeSelector(),
+    _nv;
+for (var i = 0, len = textnodes.length; i<len; i++){
+    _nv = textnodes[i].nodeValue;
+    textnodes[i].nodeValue = _nv.replace(/E/g,'\uE00F');
+}
+
+var textnodes = nativeSelector(),
+    _nv;
+for (var i = 0, len = textnodes.length; i<len; i++){
+    _nv = textnodes[i].nodeValue;
+    textnodes[i].nodeValue = _nv.replace(/o</g,'\uE036');
+}
