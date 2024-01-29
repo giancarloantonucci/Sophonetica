00b47d3a8d9e29024f84116f5e2185336df9fea0
Giancarlo A. Antonucci <antonucci@maths.ox.ac.uk>
2024-01-29 19:39:24 +0000

    Update render.js

--- render.js
+++ render.js
@@ -56,7 +56,7 @@ for (var i = 0, len = textnodes.length; i<len; i++){
 
 for (var i = 0, len = textnodes.length; i<len; i++){
     _nv = textnodes[i].nodeValue;
-    textnodes[i].nodeValue = _nv.replace(/⍵/g,'\uE008');
+    textnodes[i].nodeValue = _nv.replace(/ω/g,'\uE008');
 }
 
 for (var i = 0, len = textnodes.length; i<len; i++){
